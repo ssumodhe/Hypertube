6691347239696148a7131794277b1f1b1929713e
Safaiya SUMODHEE <ssumodhe@e2r1p14.42.fr>
2018-02-19 16:35:32 +0100

    building Home page w/ redirect to video page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+
+
+
+
+
     {{note}}
+
+    <div id="list_movies" v-for="lib in library">
+      <div class="list-group col-md-6 col-md-offset-3" >
+        <router-link to="/video" class="list-group-item list-group-item-action flex-column align-items-start" >
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{lib.id}}</h5>
+            <small>3 days ago</small>
+          </div>
+          <p class="mb-1">{{lib.title}}</p>
+          <small>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</small>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,8 +28,15 @@ export default{
   name: 'home',
   data(){
     return {
-      note: "This is the Home page Where all videos will be displayed"
+      note: "This is the Home page Where all videos will be displayed",
+      library : []
     }
+  },
+  created: function(){
+    this.$http.get('https://jsonplaceholder.typicode.com/albums')
+      .then(function(response){
+        this.library = response.data;
+      })
   }
 }
 </script>
--- src/components/Video.vue
+++ src/components/Video.vue
@@ -7,17 +7,11 @@
     <br>
     <br>
 
-    <div>
-      <ul>
-        <li v-for="lib in library">
-          <span>{{lib.id}}: {{lib.title}}</span>
-        </li>
-      </ul>
-    </div>
+    <video controls>
+<!--      <source src="http://e2r3p14.42.fr:5555/video" type="video/mp4"></source>  -->
+      <source src="" type="video/mp4"></source> 
+    </video>
 
-    <!-- <video controls>
-     <source src="http://e2r3p14.42.fr:5555/video" type="video/mp4"></source> 
-     </video> -->
   </div>
 </template>
 
@@ -27,33 +21,32 @@ export default{
   data(){
     return {
       note: "This is the Streaming page !",
-      library : []
+
     }
   },
   created: function(){
-    this.$http.get('https://jsonplaceholder.typicode.com/albums')
-      .then(function(response){
-        this.library = response.data;
-      })
 
-    axios({
-      method: 'post',
-      url: 'http://192.168.99.100:3000/auth/sign_in',
-      data: {
-        email: 'tpayet@student.42.fr',
-        password: 'QWErty123'
-      }
-    })
-      .then(function (response) {
-        console.log("response's data:");
-        console.log(response.data);
-        console.log("response's header:");
-        console.log(response.headers);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    // axios({
+    //   method: 'post',
+    //   url: 'http://192.168.99.100:3000/auth/sign_in',
+    //   data: {
+    //     email: 'tpayet@student.42.fr',
+    //     password: 'QWErty123'
+    //   }
+    // })
+    //   .then(function (response) {
+    //     console.log("response's data:");
+    //     console.log(response.data);
+    //     console.log("response's header:");
+    //     console.log(response.headers);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
   }
 }
 
 </script>
+
+<style scoped>
+</style>
