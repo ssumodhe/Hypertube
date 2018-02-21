7bd1b4ec60457b8ff2ee7e0388c986e35245f482
Safaiya SUMODHEE <ssumodhe@e2r9p20.42.fr>
2018-02-21 16:23:32 +0100

    middleware: beforeEnter set in routes ; needs to be defined

--- frontend_app/src/router/index.js
+++ frontend_app/src/router/index.js
@@ -16,6 +16,10 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+      // MiddleWare
+      // beforeEnter: (to, from, next) => {
+      //   // ...
+      // }
     },
     {
       path: '/',
