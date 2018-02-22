fb5f7da690d98b79b0deb91a85f7eb2c72343477
KnoX09 <aymeric95@hotmail.fr>
2018-02-22 22:50:57 +0100

    starting user page

--- frontend_app/src/components/User.vue
+++ frontend_app/src/components/User.vue
@@ -2,6 +2,63 @@
   <div class="user">
     <h1>User Page</h1>
     {{note}}
+
+    <div class="row">
+      <div class="col-md-2 col-md-offset-3 idcard">
+        <img class="img-circle" src="/static/img/emoji_kitty.png" width="100px">
+
+      </div>
+      <div class="col-md-4 idcard">
+        <div id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="card">
+    <div class="card-header" role="tab" id="headingOne">
+      <h5 class="mb-0">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </a>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne">
+      <div class="card-block">
+        Modif input
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingTwo">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </a>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="card-block">
+        Modif input
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" role="tab" id="headingThree">
+      <h5 class="mb-0">
+        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </a>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="card-block">
+        Modif input
+      </div>
+    </div>
+  </div>
+</div>
+
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -15,3 +72,11 @@ export default{
   }
 }
 </script>
+
+<style scoped>
+  .idcard{
+    border: 1px solid black;
+
+  }
+
+</style>
