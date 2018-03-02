aeef1b45096606cd303112ab00b0baaadf05ca66
Safaiya SUMODHEE <ssumodhe@e2r6p16.42.fr>
2018-03-02 16:57:07 +0100

    404 page : created + set in middleware (src/main.js) for redirections

--- frontend_app/src/router/index.js
+++ frontend_app/src/router/index.js
@@ -6,6 +6,7 @@ import Login from '@/components/login'
 import Home from '@/components/home'
 import User from '@/components/user'
 import Movie from '@/components/video'
+import NotFound from '@/components/404NotFound'
 
 Vue.use(Router)
 Vue.use(vueResource)
@@ -16,12 +17,12 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login,
-      beforeEnter: (to, from, next) => {
-        console.log(to)
-        console.log(from)
-        console.log(next)
-        next()
-      }
+      // beforeEnter: (to, from, next) => {
+      //   console.log(to)
+      //   console.log(from)
+      //   console.log(next)
+      //   next()
+      // }
       // MiddleWare
       // beforeEnter: (to, from, next) => {
       //   // ...
@@ -41,6 +42,11 @@ export default new Router({
       path: '/video',
       name: 'movie',
       component: Movie
+    },
+    {
+      path: '/404',
+      name: 'notFound',
+      component: NotFound
     }
   ],
   mode: 'history'
