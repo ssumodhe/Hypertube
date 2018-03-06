8f7b3017615c030b87ae66ee96c9977f32581339
Safaiya SUMODHEE <ssumodhe@e2r8p21.42.fr>
2018-03-06 10:56:58 +0100

    App : navbar collapse when small device OK + nicer display w/ footer

--- frontend_app/src/App.vue
+++ frontend_app/src/App.vue
@@ -4,20 +4,38 @@
 
     <div class="container">
       <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="navbar-header">
-          <router-link to="/" class="navbar-brand">HyperTube</router-link>
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>                        
+            </button>
+            <div class="navbar-header">
+              <router-link to="/" class="navbar-brand">HyperTube</router-link>
+            </div>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li><router-link to="/">Home</router-link></li>
+              <li><router-link to="/user">Profile</router-link></li>
+              <li><router-link to="/video">Videos</router-link></li>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Page 1-1</a></li>
+                  <li><a href="#">Page 1-2</a></li>
+                  <li><a href="#">Page 1-3</a></li>
+                </ul>
+              </li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+              <li role="separator" class="divider"></li>
+              <li><router-link to="/login">Connexion</router-link></li>
+            </ul>
+          </div>
         </div>
-
-        <ul class="nav navbar-nav">
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/user">Profile</router-link></li>
-          <li><router-link to="/video">Videos</router-link></li>
-        </ul>
-
-        <ul class="nav navbar-nav navbar-right nav-login">
-          <li role="separator" class="divider"></li>
-          <li><router-link to="/login">Connexion</router-link></li>
-        </ul>
       </nav>
     </div>
 
@@ -39,9 +57,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 10%;
+  margin-bottom: 10%;
+  width: 100%;
 }
 router-view {
   margin-top: 20px;
+}
+#connexion_button{
+  padding-right: 20px;
 }
 </style>
 
