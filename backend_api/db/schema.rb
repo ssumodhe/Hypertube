ce036c0bf21e826915b1cd6e5d470fbf57f28e66
Thomas Payet <tpayet@student.42.fr>
2018-03-08 15:57:50 +0100

    Add migration for subtitles in videos table

--- backend_api/db/schema.rb
+++ backend_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180302150952) do
+ActiveRecord::Schema.define(version: 20180308142837) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20180302150952) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subtitles_fr"
+    t.string "subtitles_en"
     t.index ["token"], name: "index_videos_on_token", unique: true
   end
 
