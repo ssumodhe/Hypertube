7cd00a13a9f2fb234dc1d457632230d5cce22626
Thomas Payet <tpayet@student.42.fr>
2018-03-10 20:26:19 +0100

    Add authentication on comments post & put

--- backend_api/app/controllers/comments_controller.rb
+++ backend_api/app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :update, :destroy]
-  # before_action :logged_in?
+  before_action :logged_in?
 
   # GET /comments
   def index
