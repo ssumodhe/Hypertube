2669922d5a3b2005106aafd045336af891231835
Thomas Payet <tpayet@student.42.fr>
2018-03-10 20:48:24 +0100

    Add hypertube.tpayet.com as origin for cors

--- backend_api/config/initializers/cors.rb
+++ backend_api/config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080'
+    origins 'https://hypertube.tpayet.com'
 
     resource '*',
       headers: :any,
