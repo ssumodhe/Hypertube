0a08e825811506f5cf33d404739bb3c7ba1ef46f
Safaiya SUMODHEE <ssumodhe@e2r8p10.42.fr>
2018-03-11 16:56:04 +0100

    Video page : comment section ADDED (not functionnal)

--- frontend_app/src/components/Video.vue
+++ frontend_app/src/components/Video.vue
@@ -11,12 +11,49 @@
       <source src="https://mdbootstrap.com/img/video/Tropical.mp4" type="video/mp4"></source> 
     </video>
 
+
 <!--     <video autoplay loop muted="true" controls class="video-js">
       <source
         src="http://e1r5p16.42.fr:5555/video/YmsorVS9x4x1YA4abxEV3LQ7LAoDH2xXXTjgyMcZWeXdYPmf1xutJJcSEQWESRRMMDSDaQQwXWR8JsK6tSkjpakbPKQszXLx1Tfu4EXDarC1Gk6xxdY6j5t5eL3NJihJPsrQBmmf6BtT1R42uLvHWxvYfBxJwJH6R7hCxRAbvQjTphupyj92" type="video/mp4">
       <track kind="subtitles" src="http://localhost:8080/static/sub.vtt" srclang="en" label="English" default="">
       <track kind="subtitles" src="http://e1r5p16.42.fr:5555/sub.vtt" srclang="fr" label="French" default="">
     </video> -->
+
+    <div class="comments">
+      <div class="comment-wrap">
+          <div class="photo">
+              <div class="avatar" style="background-image: url('https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/128.jpg')"></div>
+          </div>
+          <div class="comment-block">
+              <form action="">
+                  <textarea name="" id="" cols="30" rows="3" placeholder="Add comment..."></textarea>
+              </form>
+          </div>
+      </div>
+
+      <div class="container" v-for="comment in comments">
+        <div class="row">
+
+          <div class="col-sm-1">
+            <div class="thumbnail">
+              <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
+            </div>
+          </div>
+
+          <div class="col-sm-5">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <strong>{{comment.title}}</strong> <span class="text-muted">commented 5 days ago</span>
+              </div>
+              <div class="panel-body">
+              {{comment.body}}
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+  </div>
 
   </div>
 </template>
@@ -27,10 +64,22 @@ export default{
   data(){
     return {
       note: "This is the Streaming page !",
+      comments: [],
 
     }
   },
   created: function(){
+
+    axios({
+      method: 'get',
+      url: 'https://jsonplaceholder.typicode.com/posts'
+      })
+      .then( (response) => {
+        this.comments = response.data
+      })
+      .catch( (error) => {
+        console.log(error)
+      });
 
     // axios({
     //   method: 'post',
@@ -55,6 +104,9 @@ export default{
 </script>
 
 <style scoped>
+  .movie{
+    width: 100%;
+  }
   video{
     border: 1px solid black;
     width: 100%;
