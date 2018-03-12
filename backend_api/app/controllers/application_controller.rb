ee8a4f5b334c0e6f585bfd9f246a5281afbea0d0
Thomas Payet <tpayet@student.42.fr>
2018-03-12 22:58:05 +0100

    reset params configuration for user by devise

--- backend_api/app/controllers/application_controller.rb
+++ backend_api/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
 
   protected
