54234805792bd76b6cf631adc9da35abf939faf8
Thomas Payet <tpayet@student.42.fr>
2018-03-13 16:42:35 +0100

    Update routes for delete route

--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'search', to: 'search#query'
 
-  resources :videos, except: [:update, :destroy]
+  resources :videos, except: [:update]
   get 'videos/:id/comments', to: 'videos#comments'
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
