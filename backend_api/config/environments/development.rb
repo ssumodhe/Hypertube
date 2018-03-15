2a0bfc687ee5cae33e49edaf6b714d85b7567f4c
Thomas Payet <tpayet@student.42.fr>
2018-03-15 15:24:07 +0100

    Remove useless mail default url options

--- backend_api/config/environments/development.rb
+++ backend_api/config/environments/development.rb
@@ -45,6 +45,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.signup_redirect_url = 'http://localhost:3000'
+  config.signup_redirect_url = 'https://hypertube.tpayet.com'
 end
