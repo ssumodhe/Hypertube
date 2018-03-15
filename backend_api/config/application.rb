d379153180135c35704bde69627577075b3a5ed9
Thomas Payet <tpayet@student.42.fr>
2018-03-15 15:22:28 +0100

    Add rack:session:cookie usage

--- backend_api/config/application.rb
+++ backend_api/config/application.rb
@@ -29,5 +29,7 @@ module BackendApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use Rack::Session::Cookie, secret: 'woots'
   end
 end
