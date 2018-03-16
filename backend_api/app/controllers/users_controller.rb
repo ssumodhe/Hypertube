122d9375b2a534601019b2e6d2f9fd441fdc575e
Thomas Payet <tpayet@student.42.fr>
2018-03-16 20:01:37 +0100

    wip paperclip integration

--- backend_api/app/controllers/users_controller.rb
+++ backend_api/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-  before_action :logged_in?
+  # before_action :logged_in?
 
   # GET /users
   def index
--- backend_api/app/models/user.rb
+++ backend_api/app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
           :omniauthable
   include DeviseTokenAuth::Concerns::User
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   has_many :comments
 end
