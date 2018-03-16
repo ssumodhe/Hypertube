08e237841b90d22aa8fe3fd0e12e55a86b340362
Safaiya SUMODHEE <ssumodhe@e6r3p2.vp.42.fr>
2018-03-16 18:15:00 +0100

    login page : when signing in -> get headers var

--- frontend_app/src/components/Login.vue
+++ frontend_app/src/components/Login.vue
@@ -71,7 +71,14 @@ export default{
         }
       })
         .then( (response) => {
+          console.log(response.headers)
           localStorage.setItem('token', response.headers['access-token'])
+          localStorage.setItem('client', response.headers['client'])
+          localStorage.setItem('expiry', response.headers['expiry'])
+          localStorage.setItem('token-type', response.headers['token-type'])
+          localStorage.setItem('uid', response.headers['uid'])
+
+
           localStorage.setItem('id', response.data.data['id'])
           localStorage.setItem('email', response.data.data['email'])
           localStorage.setItem('picture', response.data.data['picture'])
