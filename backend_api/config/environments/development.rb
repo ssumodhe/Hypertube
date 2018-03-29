048b4ae77d8cd3cdecfb98aa39d34db672325de6
Thomas Payet <tpayet@student.42.fr>
2018-03-29 17:06:58 +0200

    WIP mailer config

--- backend_api/config/environments/development.rb
+++ backend_api/config/environments/development.rb
@@ -46,5 +46,21 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentification:     :login,
+    enable_starttls_auto: true,
+    tls:                  false,
+    domain:               'gmail.com',
+    openssl_verify_mode:  'none'
+  }
+
   config.signup_redirect_url = 'http://localhost:3000'
 end
