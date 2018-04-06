8961e99b066bf9fa47f8d1b10d6942d8e3eee354
Safaiya SUMODHEE <ssumodhe@e6r3p2.vp.42.fr>
2018-04-06 14:40:42 +0200

    Forgotten Password Page : DONE | need backendapi's correct url

--- frontend_app/src/components/ForgotPassword.vue
+++ frontend_app/src/components/ForgotPassword.vue
@@ -2,6 +2,8 @@
 	<div class="password">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-4">
+        <div v-if="successMsg" class="alert alert-success" role="alert">{{successMsg}}
+            </div>
 			<small><mark v-lang.msg_pswd></mark></small>
             <form @submit.prevent="submitPwd">
 
@@ -22,12 +24,10 @@
 </template>
 
 <script>
+import {signUpUrl} from '@/config.js'
+
 export default{
   name: 'password',
-  created: function(){
-    console.log('reset token')
-    console.log(this.$route.params['reset_password_token'])
-  },
   computed:{
     new_pswd()  {
      return this.translate('new_pswd')
@@ -40,7 +40,8 @@ export default{
     return{
     	newPwdSuccessClass: '',
       	cfmPwdSuccessClass: '',
-      	pwdSubmitBtnDisabled: true
+      	pwdSubmitBtnDisabled: true,
+        successMsg: ''
     }
   },
   methods:{
@@ -75,7 +76,29 @@ export default{
     }
   	},
   	submitPwd: function(){
-  		// send
+  		axios({
+        method: 'put',
+        url: signUpUrl,
+        headers: {
+          'Content-Type': 'application/json',
+          'access-token': this.$route.query.token,
+          'client': this.$route.query.client_id,
+          'expiry': this.$route.query.expiry,
+          'token-type': 'Bearer',
+          'uid': this.$route.query.uid
+        },
+        data: {
+          'password': this.$refs.txtNewPwd.value,
+          'password_confirmation': this.$refs.txtCfmPwd.value
+        }
+      })
+      .then( (response) => {
+        this.successMsg = 'Your password has been correctly modified, you will be redirect to the login page to proceed with the sing in process.'
+        setTimeout( () => this.$router.push({ path: '/login'}), 5000);
+      })
+      .catch( (error) => {
+        console.log(error)
+      });
   	}
   }
 }
