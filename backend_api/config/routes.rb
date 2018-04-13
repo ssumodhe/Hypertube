6aeab649e99bae425fb5f56b1e8bea83c45100ad
Thomas Payet <tpayet@student.42.fr>
2018-04-13 15:06:46 +0200

    Update video performance controller to have real video u_u

--- backend_api/config/routes.rb
+++ backend_api/config/routes.rb
@@ -14,9 +14,8 @@ Rails.application.routes.draw do
     get :comments
     get :perform
   end
-  # get 'videos/:id/comments', to: 'videos#comments'
+
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     omniauth_callbacks: 'overrides/omniauth_callbacks'
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
